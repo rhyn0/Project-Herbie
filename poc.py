5992e1027404037dbd27f9bc61086581ae8bb2fc
Ryan Ozawa <ryan.ozawa21@gmail.com>
2021-03-29 10:07:49 -0700

    changed to test new encoder setup

--- poc.py
+++ poc.py
@@ -10,6 +10,11 @@ POC_ADDR = 0x80
 QUAD_CR = CornerMotor(rc, POC_ADDR, 2)
 QUAD_DR = Motor(rc, POC_ADDR, 1)
 
+#have to do some roundabout calibration, since code is set for absolute motors still
+QUAD_CR.encoders_per_degree = 8300 / 360 #testing showed it was about 8300 for a full revolution
+QUAD_CR.calibrated = True
+while True:
+    QUAD_CR.rotate_n_degrees("right", 45)
+    QUAD_CR.rotate_n_degrees("left", 45)
 
-QUAD_CR.move_distance(0.1, 0.1)
-QUAD_DR.move_distance(0.1, 0.1)
+
